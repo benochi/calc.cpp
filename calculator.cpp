6e15c214db92e6308e13f900f45f0a2fc3a3c935
Dan P <73758508+benochi@users.noreply.github.com>
2021-07-25 16:29:53 -0500

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch(oper)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            return 0;
+    }
+}
